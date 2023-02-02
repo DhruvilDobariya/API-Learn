9227265b772dd1a6712cd96b213c557cdf850750
Dhruvil Dobariya <190540107051@darshan.ac.in>
2023-02-02 22:16:21 +0530

    Create new repository and migrate content of API Learning directory form dotnet directory to API Learn directory

---
+++ code/Phase 3/Working with collections/GenericCollections/QueueProperties.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericCollections
+{
+    public class QueueProperties
+    {
+        public static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+
+            queue.Enqueue("Dhruvil");
+            queue.Enqueue("Dhaval");
+            queue.Enqueue("Jenil");
+            queue.Enqueue("Bhargav");
+
+            Console.WriteLine(queue.Count);
+        }
+    }
+}
---
+++ code/Phase 3/Working with collections/GenericCollections/StackProperties.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericCollections
+{
+    public class StackProperties
+    {
+        public static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push("Dhruvil");
+            stack.Push("Dhaval");
+            stack.Push("Dhruv");
+            stack.Push("Jenil");
+
+            Console.WriteLine(stack.Count);
+        }
+    }
+}
